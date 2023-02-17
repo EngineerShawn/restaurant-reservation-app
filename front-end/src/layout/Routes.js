71b5e938a03016a164671f4252d8d75a4910e8e0
BountyFiveO <bountyfiveo@gmail.com>
2023-02-17 14:10:11 -0500

    Bulk changes for dev of app

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -1,9 +1,13 @@
 import React from "react";
-
 import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
+import ReservationCreate from "../Reservations/NewReservation";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
+import Seat from "../Reservations/Seat";
+import Search from "../Search/Search";
+import EditReservation from "../Reservations/EditReservation";
+import CreateTable from "../Tables/CreateTable";
 
 /**
  * Defines all the routes for the application.
@@ -13,22 +17,43 @@ import { today } from "../utils/date-time";
  * @returns {JSX.Element}
  */
 function Routes() {
-  return (
-    <Switch>
-      <Route exact={true} path="/">
-        <Redirect to={"/dashboard"} />
-      </Route>
-      <Route exact={true} path="/reservations">
-        <Redirect to={"/dashboard"} />
-      </Route>
-      <Route path="/dashboard">
-        <Dashboard date={today()} />
-      </Route>
-      <Route>
-        <NotFound />
-      </Route>
-    </Switch>
-  );
+	return (
+		<Switch>
+			<Route
+				exact={true}
+				path="/"
+			>
+				<Redirect to={"/dashboard"} />
+			</Route>
+			<Route
+				exact={true}
+				path="/reservations"
+			>
+				<Redirect to={"/dashboard"} />
+			</Route>
+			<Route path="/reservations/:reservation_id/seat">
+				<Seat />
+			</Route>
+			<Route path="/reservations/:reservation_id/edit">
+				<EditReservation />
+			</Route>
+			<Route path="/reservations/new">
+				<ReservationCreate />
+			</Route>
+			<Route path="/dashboard">
+				<Dashboard date={today()} />
+			</Route>
+			<Route path="/tables/new">
+				<CreateTable />
+			</Route>
+			<Route path="/search">
+				<Search />
+			</Route>
+			<Route>
+				<NotFound />
+			</Route>
+		</Switch>
+	);
 }
 
 export default Routes;
