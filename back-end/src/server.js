0f9aaa18919f03bdd4f1acb7fd918e8ef3a96888
BountyFiveO <bountyfiveo@gmail.com>
2023-02-18 09:43:04 -0500

    Revert "Bulk changes for dev of app"
    This reverts commit 71b5e938a03016a164671f4252d8d75a4910e8e0.

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -6,9 +6,11 @@ const knex = require("./db/connection");
 knex.migrate
   .latest()
   .then((migrations) => {
+    console.log("migrations", migrations);
     app.listen(PORT, listener);
   })
   .catch((error) => {
+    console.error(error);
     knex.destroy();
   });
 
--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,11 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
 import { listReservations } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
-import { formatAsDate, previous, next, today } from "../utils/date-time";
-import useQuery from "../utils/useQuery";
-import DisplayReservations from "./DisplayReservations";
-import TableList from "./TableList";
 
 /**
  * Defines the dashboard page.
@@ -14,86 +9,30 @@ import TableList from "./TableList";
  * @returns {JSX.Element}
  */
 function Dashboard({ date }) {
-	let isToday = true;
-	const query = useQuery();
-	const getDate = query.get("date");
+  const [reservations, setReservations] = useState([]);
+  const [reservationsError, setReservationsError] = useState(null);
 
-	if (getDate && getDate !== today()) {
-		date = getDate;
-		isToday = false;
-	}
+  useEffect(loadDashboard, [date]);
 
-	const [reservations, setReservations] = useState([]);
-	const [reservationsError, setReservationsError] = useState(null);
+  function loadDashboard() {
+    const abortController = new AbortController();
+    setReservationsError(null);
+    listReservations({ date }, abortController.signal)
+      .then(setReservations)
+      .catch(setReservationsError);
+    return () => abortController.abort();
+  }
 
-	useEffect(loadDashboard, [date]);
-
-	function loadDashboard() {
-		const controller = new AbortController();
-		setReservationsError(null);
-		listReservations({ date }, controller.signal)
-			.then(setReservations)
-			.catch(setReservationsError);
-		return () => controller.abort();
-	}
-
-	const displayDate = formatAsDate(date);
-
-	const history = useHistory();
-
-	const previousDate = previous(date);
-	const nextDate = next(date);
-
-	function pushDate(dateToMove) {
-		history.push(`/dashboard?date=${dateToMove}`);
-	}
-
-	let result = reservations.filter((reservation) => {
-		return (
-			reservation.status !== "finished" && reservation.status !== "cancelled"
-		);
-	});
-
-	return (
-		<main>
-			<h1>Dashboard</h1>
-			<div className="d-md-flex mb-3">
-				<h4 className="mb-0">Reservations for {displayDate}</h4>
-			</div>
-			<ErrorAlert error={reservationsError} />
-			<div
-				className="btn-group"
-				role="group"
-				aria-label="Pick a date"
-			>
-				<button
-					className="btn btn-primary"
-					onClick={() => pushDate(previousDate)}
-				>
-					Back
-				</button>
-				<button
-					className="btn btn-primary"
-					onClick={() => history.push("/dashboard")}
-					disabled={date === today()}
-				>
-					Today
-				</button>
-				<button
-					className="btn btn-primary"
-					onClick={() => pushDate(nextDate)}
-				>
-					Forward
-				</button>
-			</div>
-			<DisplayReservations
-				reservations={result}
-				isToday={isToday}
-			/>
-			{!reservations.length && <h3>No reservations on this date</h3>}
-			<TableList />
-		</main>
-	);
+  return (
+    <main>
+      <h1>Dashboard</h1>
+      <div className="d-md-flex mb-3">
+        <h4 className="mb-0">Reservations for date</h4>
+      </div>
+      <ErrorAlert error={reservationsError} />
+      {JSON.stringify(reservations)}
+    </main>
+  );
 }
 
 export default Dashboard;
