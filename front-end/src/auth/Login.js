2713e7a53c5a1a324b52ee6a89202bbdd01947b7
Shawn Patterson <147225693+EngineerShawn@users.noreply.github.com>
2024-02-08 12:59:00 -0600

    Update Login.js

--- front-end/src/auth/Login.js
+++ front-end/src/auth/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import "./authStyle.css";
 
 const Login = ({ switchToRegister }) => {
     const [email, setEmail] = useState('');
