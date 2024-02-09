86ed8427fbf8e7ea73cf2a1bbeb2cd56408c801c
Shawn Patterson <147225693+EngineerShawn@users.noreply.github.com>
2024-02-09 03:13:37 -0500

    Update authMiddleware.js

--- front-end/src/auth/authMiddleware.js
+++ front-end/src/auth/authMiddleware.js
@@ -2,20 +2,19 @@ import React, { useState } from 'react';
 import Login from './Login';
 import Register from './Register';
 
-const AuthMiddleware = () => {
-    const [showLogin, setShowLogin] = useState(true);
+const ParentComponent = () => {
+    const [showLogin, setShowLogin] = useState(false);
+    const [showRegister, setShowRegister] = useState(false);
 
-    const switchToRegister = () => setShowLogin(false);
-    const switchToLogin = () => setShowLogin(true);
+    const handleLoginClose = () => setShowLogin(false);
+    const handleRegisterClose = () => setShowRegister(false);
 
     return (
         <div>
-            {showLogin ? 
-                <Login switchToRegister={switchToRegister} /> : 
-                <Register switchToLogin={switchToLogin} />
-            }
+            {showLogin && <Login handleClose={handleLoginClose} />}
+            {showRegister && <Register handleClose={handleRegisterClose} />}
         </div>
     );
 };
 
-export default AuthMiddleware;
+export default ParentComponent;
