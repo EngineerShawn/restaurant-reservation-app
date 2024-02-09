e19a8cec2cae48b3f80d6d1bb4d683e0deb93693
Shawn Patterson <147225693+EngineerShawn@users.noreply.github.com>
2024-02-09 02:33:22 -0500

    Update Register.js

--- front-end/src/auth/Register.js
+++ front-end/src/auth/Register.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import "./authStyle.css";
+import "./authMiddleware";
 
 
 const Register = ({ show, handleClose, switchToLogin }) => {
