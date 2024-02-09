70bd44572a86125b0439c016b8d0dab967cc6349
Shawn Patterson <147225693+EngineerShawn@users.noreply.github.com>
2024-02-09 18:23:53 -0500

    Update Menu.js

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -10,32 +10,25 @@ import ParentComponent from '../auth/ParentComponent';
  */
 
 function Menu() {
-
-  // State to track if the user is logged in
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // State to store user's employee ID
   const [employeeId, setEmployeeId] = useState('');
+  const [showLogin, setShowLogin] = useState(false);
 
-  // Function to handle successful login
   const handleLogin = (id) => {
     setIsLoggedIn(true);
     setEmployeeId(id);
   };
 
   return (
-    <nav className="navbar navbar-dark align-items-start p-0">
-      <div className="container-fluid d-flex flex-column p-0">
-        <Link
-          className="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0"
-          to="/"
-        >
-          <div className="sidebar-brand-text text-wrap mx-3">
+    <nav className="navbar">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
+          <div className="sidebar-brand-text">
             <span>EngineerPattersons Reservation App</span>
           </div>
         </Link>
-        <hr className="sidebar-divider my-0" />
-        <ul className="nav navbar-nav text-light" id="accordionSidebar">
-          {/* If user is not logged in, show Login/Register link */}
+        <hr />
+        <ul className="nav navbar-nav">
           {!isLoggedIn && (
             <li className="nav-item">
               <Link className="nav-link" onClick={() => setShowLogin(true)}>
@@ -44,7 +37,6 @@ function Menu() {
               </Link>
             </li>
           )}
-          {/* If user is logged in, show user's employee ID */}
           {isLoggedIn && (
             <li className="nav-item">
               <span className="nav-link text-success">
@@ -52,7 +44,6 @@ function Menu() {
               </span>
             </li>
           )}
-          {/* Render the Login/Register component if showLogin state is true */}
           {showLogin && <ParentComponent handleLogin={handleLogin} />}
           <li className="nav-item">
             <Link className="nav-link" to="/dashboard">
@@ -79,15 +70,11 @@ function Menu() {
             </Link>
           </li>
         </ul>
-        <div className="text-center d-none d-md-inline">
-          <button
-            className="btn rounded-circle border-0"
-            id="sidebarToggle"
-            type="button"
-          />
+        <div className="text-center">
+          <button className="btn rounded-circle border-0" type="button" />
         </div>
       </div>
-    </nav>         
+    </nav>
   );
 }
 
