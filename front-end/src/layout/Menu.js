8ebd1ee31801a76684d76f3f9c13a17da9ccc544
Shawn Patterson <147225693+EngineerShawn@users.noreply.github.com>
2024-02-09 03:51:22 -0500

    Update Menu.js

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -1,4 +1,4 @@
-import React, { UseState } from "react";
+import React, { useState } from "react";
 
 import { Link } from "react-router-dom";
 import ParentComponent from '../auth/ParentComponent';
