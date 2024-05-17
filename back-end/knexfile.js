358f13f526746c86dcb57528bb9a1f839af875eb
Shawn Patterson <147225693+EngineerShawn@users.noreply.github.com>
2024-05-16 23:28:14 -0400

    Update knexfile.js

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -6,8 +6,8 @@ require('dotenv').config();
 const path = require("path");
 
 const {
-  DATABASE_URL = process.env.DB_URL,
-  DATABASE_URL_DEVELOPMENT = process.env.DB_URL_DEV,
+  DATABASE_URL = "postgres://fwrmqktn:hx8eNz4SMD5vXj9UyEBOMUhDMF5T-t5L@fanny.db.elephantsql.com/fwrmqktn", //process.env.DB_URL,
+  DATABASE_URL_DEVELOPMENT = "postgres://fwrmqktn:hx8eNz4SMD5vXj9UyEBOMUhDMF5T-t5L@fanny.db.elephantsql.com/fwrmqktn",         // process.env.DB_URL_DEV,
   DATABASE_URL_TEST = process.env.DB_URL_TEST,
   DATABASE_URL_PREVIEW = process.env.DB_URL_PREVIEW,
   DEBUG,
@@ -16,7 +16,7 @@ module.exports = {
   development: {
     client: "postgresql",
     pool: { min: 1, max: 5 },
-    connection: DATABASE_URL_DEVELOPMENT,
+    connection: process.env.DATABASE_URL_DEVELOPMENT || "postgres://fwrmqktn:hx8eNz4SMD5vXj9UyEBOMUhDMF5T-t5L@fanny.db.elephantsql.com/fwrmqktn",
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
@@ -52,7 +52,7 @@ module.exports = {
   production: {
     client: "postgresql",
     pool: { min: 1, max: 5 },
-    connection: DATABASE_URL,
+    connection: process.env.DATABASE_URL || "postgres://fwrmqktn:hx8eNz4SMD5vXj9UyEBOMUhDMF5T-t5L@fanny.db.elephantsql.com/fwrmqktn",
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
